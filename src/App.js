3178580178aeafdb883ddea2700feb3dd6260825
Renan Andrade <Fel9.renan02@gmail.com>
2022-02-27 20:29:11 -0300

    Adicionado provedor de contexto de autenticação

--- src/App.js
+++ src/App.js
@@ -6,26 +6,29 @@ import { Navigate, Route, Routes } from "react-router";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import useAuth from "./hooks/useAuth";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   const currentUser = useAuth();
   return (
     <Routes>
-      <Route
-        path="/"
-        element={
-          // se não houver usuário logado, redireciona ao login
-          currentUser !== null ? (
-            <div className="flex flex-row">
-              <Sidebar />
-              <TextChats></TextChats>
-              <MessageArea></MessageArea>
-            </div>
-          ) : (
-            <Navigate to={"/login"} replace />
-          )
-        }
-      />
+      <AuthContextProvider>
+        <Route
+          path="/"
+          element={
+            // se não houver usuário logado, redireciona ao login
+            currentUser !== null ? (
+              <div className="flex flex-row">
+                <Sidebar />
+                <TextChats></TextChats>
+                <MessageArea></MessageArea>
+              </div>
+            ) : (
+              <Navigate to={"/login"} replace />
+            )
+          }
+        />
+      </AuthContextProvider>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
     </Routes>
