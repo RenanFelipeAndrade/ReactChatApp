7cfd94f35dd50598d962dea468f6686bd204186d
Renan Andrade <Fel9.renan02@gmail.com>
2022-03-10 11:33:56 -0300

    Alterado elementos div para ul e li

--- src/components/ChatList.js
+++ src/components/ChatList.js
@@ -21,34 +21,38 @@ export const ChatList = ({
     }
   }
 
-  return chats.map((chat, index) => (
-    <div key={index}>
-      {chat.name === activeChat.name ? (
-        // quando o chat for o ativo
-        <button
-          onClick={() => setActiveChat({ index: index, ...chat })}
-          className="chat-button chat-active"
-        >
-          <span className="chat-name">{chat.name}</span>
-          {/* cria um botão de excluir chat */}
+  return (
+    <ul className="chat-list">
+      {chats.map((chat, index) => (
+        <li key={index}>
+          {chat.name === activeChat.name ? (
+            // quando o chat for o ativo
+            <button
+              onClick={() => setActiveChat({ index: index, ...chat })}
+              className="chat-button chat-active"
+            >
+              <span className="chat-name">{chat.name}</span>
+              {/* cria um botão de excluir chat */}
 
-          <span className="group" onClick={deleteChat}>
-            <TrashIcon className="w-4 h-4" />
-            {/* aparece quando der hover no ícone de lixeira */}
-            <small className="absolute transition-all bg-zinc-800 -top-5 rounded shadow px-1 right-0 hidden group-hover:block">
-              Remover Chat
-            </small>
-          </span>
-        </button>
-      ) : (
-        // quando o chat não for ativo
-        <button
-          onClick={() => setActiveChat({ index: index, ...chat })}
-          className="chat-button"
-        >
-          <span className="chat-name">{chat.name}</span>
-        </button>
-      )}
-    </div>
-  ));
+              <span className="group" onClick={deleteChat}>
+                <TrashIcon className="w-4 h-4" />
+                {/* aparece quando der hover no ícone de lixeira */}
+                <small className="absolute transition-all bg-zinc-800 -top-5 rounded shadow px-1 right-0 hidden group-hover:block">
+                  Remover Chat
+                </small>
+              </span>
+            </button>
+          ) : (
+            // quando o chat não for ativo
+            <button
+              onClick={() => setActiveChat({ index: index, ...chat })}
+              className="chat-button"
+            >
+              <span className="chat-name">{chat.name}</span>
+            </button>
+          )}
+        </li>
+      ))}
+    </ul>
+  );
 };
