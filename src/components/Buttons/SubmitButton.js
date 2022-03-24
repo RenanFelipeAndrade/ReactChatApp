b46052dfd58c746a1181a27fd198739b3ea51e11
Renan Andrade <Fel9.renan02@gmail.com>
2022-03-24 08:19:56 -0300

    Alterado largura do botão

--- src/components/Buttons/SubmitButton.js
+++ src/components/Buttons/SubmitButton.js
@@ -1,7 +1,7 @@
 export const SubmitButton = ({ children, onClick }) => {
   return (
     <button
-      className="px-2 py-1 text-sm bg-teal-500 rounded-sm hover:bg-teal-700 transition w-fit"
+      className="px-2 py-1 text-sm bg-teal-500 rounded-sm hover:bg-teal-700 transition w-full"
       onClick={onClick}
       type={"submit"}
     >
