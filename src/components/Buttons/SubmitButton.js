22c7cc89f248215149e93dfba5c6fb2cff866c70
Renan Andrade <Fel9.renan02@gmail.com>
2022-03-24 08:09:29 -0300

    Adicionado componente botão de envio

---
+++ src/components/Buttons/SubmitButton.js
@@ -0,0 +1,11 @@
+export const SubmitButton = ({ children, onClick }) => {
+  return (
+    <button
+      className="px-2 py-1 text-sm bg-teal-500 rounded-sm hover:bg-teal-700 transition w-fit"
+      onClick={onClick}
+      type={"submit"}
+    >
+      {children}
+    </button>
+  );
+};
