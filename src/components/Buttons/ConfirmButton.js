717b029090a15d8fc1ae6733458950b6dfef529a
Renan Andrade <Fel9.renan02@gmail.com>
2022-03-24 12:11:29 -0300

    Alterado nome do componente; Adicionado tipo button

--- src/components/Buttons/ConfirmButton.js
+++ src/components/Buttons/ConfirmButton.js
@@ -1,9 +1,9 @@
-export const SubmitButton = ({ children, onClick }) => {
+export const ConfirmButton = ({ children, onClick, submit }) => {
   return (
     <button
       className="px-2 py-1 text-sm bg-teal-500 rounded-sm hover:bg-teal-700 transition w-full"
       onClick={onClick}
-      type={"submit"}
+      type={submit || "button"}
     >
       {children}
     </button>
