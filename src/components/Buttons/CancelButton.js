7a7e018e03a29c72175a6d02f539bf0fac831abb
Renan Andrade <Fel9.renan02@gmail.com>
2022-03-25 07:55:08 -0300

    Adicionado propriedades opcionais w-fit e margin-x-auto para botão

--- src/components/Buttons/CancelButton.js
+++ src/components/Buttons/CancelButton.js
@@ -1,7 +1,9 @@
-export const CancelButton = ({ children, onClick }) => {
+export const CancelButton = ({ children, onClick, fitWidth, centered }) => {
   return (
     <button
-      className="px-2 py-1 text-sm bg-red-500 rounded-sm hover:bg-red-700 transition w-full"
+      className={`px-2 py-1 text-sm bg-red-500 rounded-sm hover:bg-red-700 transition ${
+        fitWidth ? "w-fit" : "w-full"
+      } ${centered && "mx-auto"}`}
       onClick={onClick}
       type={"button"}
     >
